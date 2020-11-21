bdb3fca6f7fa30d9d4eff995c20b9c6185582be5
Hs210595 <hrishikeshsaste@gmail.com>
2020-11-21 20:04:25 +0530

    Last commit

---
+++ input.c
@@ -0,0 +1,10 @@
+MVI D, 01 
+; initialize register D with 01
+MVI E, 01 
+; initialize register E with 01
+SUB D		
+; subtract value of D from A.
+JZ 2011	
+; make jump to memory location
+
+
--- m.py
+++
@@ -1,8 +0,0 @@
-def is_leap(x):
-    y= (x%4)
-    if y==0:
-        print("you have enterd Leap year")
-    else:
-        print("you have enterd Non leap year")
-a=int(input('Enter the value of a '))
-is_leap(a)
